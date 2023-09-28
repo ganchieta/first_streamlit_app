166363d6d2e8d4293696b8c98b7fbab45cc19a4f
ganchieta <43427705+ganchieta@users.noreply.github.com>
2023-09-28 17:08:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry oatmeal')
+streamlit.text('🐔 Kale,Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
